1ad3687cc9e2870b32e81009e450bb4dad677156
thangtm <thangtmse04173@fpt.edu.vn>
2018-11-06 14:46:08 +0700

    update supplier

--- backend/src/main/java/com/fpt/repository/SupplierRepository.java
+++ backend/src/main/java/com/fpt/repository/SupplierRepository.java
@@ -1,9 +1,11 @@
 package com.fpt.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fpt.entity.Supplier;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-
+	public Page<Supplier> findBySupplierNameContainingAndDeleted(String q, Boolean deleted, Pageable pageable);
 }
