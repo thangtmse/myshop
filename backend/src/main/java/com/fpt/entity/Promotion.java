965b88591e205a26157736911e274e5d7585f077
thangtm <thangtmse04173@fpt.edu.vn>
2018-12-04 17:47:32 +0700

    fix bug promotion

--- backend/src/main/java/com/fpt/entity/Promotion.java
+++ backend/src/main/java/com/fpt/entity/Promotion.java
@@ -1,26 +1,47 @@
 package com.fpt.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Proxy;
+
+@Entity
+@Table(name = "Category")
+@Proxy(lazy = false)
 public class Promotion {
-	private Integer promotionId;
-	private Integer productId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "promotionId", nullable = false)
+	private Long promotionId;
+
+	@Column(name = "productId", nullable = false)
+	private Long productId;
+	@Column(name = "disountCode", nullable = false)
 	private String disountCode;
+	@Column(name = "discount", nullable = false)
 	private Float discount;
+	@Column(name = "createDate", nullable = false)
 	private Long createDate;
+	@Column(name = "exprieDate", nullable = false)
 	private Long exprieDate;
 
-	public Integer getPromotionId() {
+	public Long getPromotionId() {
 		return promotionId;
 	}
 
-	public void setPromotionId(Integer promotionId) {
+	public void setPromotionId(Long promotionId) {
 		this.promotionId = promotionId;
 	}
 
-	public Integer getProductId() {
+	public Long getProductId() {
 		return productId;
 	}
 
-	public void setProductId(Integer productId) {
+	public void setProductId(Long productId) {
 		this.productId = productId;
 	}
 
